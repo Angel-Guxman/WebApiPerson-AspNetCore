befab51eb349e3d4e518db790983981b65fdc41a
Angel <angelguzman10911@gmail.com>
2024-11-03 17:23:18 -0500

    Agregar archivos de proyecto.

---
+++ WebApiPerson/Controllers/EstudianteController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApiPerson.Models;
+using WebApiPerson.context;
+namespace WebApiPerson.Controllers
+{
+    [Route("api/estudiantes")]
+    [ApiController]
+    public class EstudianteController :ControllerBase
+    {
+        private readonly AppDBContext _dbContext;
+
+        public EstudianteController(AppDBContext context)
+
+        {
+            _dbContext = context;
+            
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Estudiante>>> GetEstudiante()
+        {
+            return await _dbContext.Estudiantes.ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Estudiante>> PostEstudiante(Estudiante estudiante)
+        {
+            _dbContext.Estudiantes.Add(estudiante);
+            await _dbContext.SaveChangesAsync();
+            return CreatedAtAction("GetEstudiante",new {id=estudiante.Id},estudiante);
+
+        }
+
+    }
+}
---
+++ WebApiPerson/context/AppDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApiPerson.Models;
+
+namespace WebApiPerson.context
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options ): base(options)
+        {
+            
+        }
+        public DbSet<Person> Persons {  get; set; }
+        
+        public DbSet<Estudiante> Estudiantes {  get; set; } 
+    }
+}
